cb85398e765e2dd8d7d01704e6fcddf7b6fe1f69
Hana Ahmed <Hana.ahmedbadr@student.giu-uni.de>
2026-09-22 18:32:18 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,8 @@
+const name = "Hana Ahmed"; 
+const studentId = 16008356; 
+const power = studentId * 2;
+ const warcry = "By the Giza plateau, the Sand Kraken surrenders!";
+  console.log(name); 
+  console.log(studentId); 
+  console.log(power); 
+  console.log(warcry); 
